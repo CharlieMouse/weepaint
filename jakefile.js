892c24e433c2c48228a6d51ecac62754b5e15beb
Charlie Mouse <mattbance@hotmail.com>
2013-02-28 11:45:03 +0000

    Initial support for lint

--- jakefile.js
+++ jakefile.js
@@ -1,13 +1,8 @@
-task("default", [], function () {
-    console.log("default");
+task("default", ["lint"]);
+
+desc("lint everything");
+task("lint", [], function () {
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("jakefile.js", {}, {});
 });
 
-desc("Example");
-task("example", ["dependency"], function () {
-
-    console.log("Hello");
-});
-
-task("dependency", function(){
-    console.log("dependency");
-});
