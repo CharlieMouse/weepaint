13e3897a1fd971a9fc1b26288dd4f6b3638a81bd
Charlie Mouse <mattbance@hotmail.com>
2013-02-28 17:24:07 +0000

    Update Jake and raedme

--- jakefile.js
+++ jakefile.js
@@ -3,6 +3,15 @@ task("default", ["lint"]);
 desc("lint everything");
 task("lint", [], function () {
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {}, {});
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    var options = {
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
 
